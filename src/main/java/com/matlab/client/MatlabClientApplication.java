eec56e83ee463f9090c367f26f138dbb1ef72d8c
Denis Maslakov <maslakov_denis@mail.ru>
2018-05-15 22:21:30 +0400

    init

---
+++ src/main/java/com/matlab/client/MatlabClientApplication.java
@@ -0,0 +1,20 @@
+package com.matlab.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MatlabClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatlabClientApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
